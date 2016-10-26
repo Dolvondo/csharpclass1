9712b079fc3193769af9dbad747d392bfd1d17ff
Marshall Mabee <marshallmabee@gmail.com>
2016-10-26 11:37:34 -0600

    update

---
+++ Notes.md
@@ -0,0 +1,21 @@
+csharpclass
+Notes:
+
+static - means reachable by other files, "Global" - referencing the cookie cutter
+public - Viewable by other objects "Is an access modifier"
+instancing - is a copy of an object - is the cookie, but not the same as other cookies
+class - Is the cookie cutter. And they make objects. Also used to assign attributes to other variables that reference the class
+private - private variables are not accessible outside of the script where it was made
+reference - anything that is static is referenced. Data that mention the source of data and uses it.
+constructor - is the same name as the class and is called when the class is run. When it loads into your RAM the constructor runs. Common in javascript.
+
+Strategies -
+--- Classes are objects that can store/(give away) attributes, which variables can ask for and assign themselves a default attribute. Another file will be for actually setting the proper
+stats for the variable. This reduces repatitive clutter.
+
+
+Errors:
+If anything doesnt make sense reboot Visual studio
+
+- Object reference not set to an instance of an objects
+    * A function is not declared with "  = new function();  "
--- Scripts/game.cs
+++ Scripts/game.cs
@@ -1,14 +1,24 @@
-
+using System;
 public class Game { //This is the cookie cutter
     public Game() { //"Game" Represents a constructor. This runs when the game class is created.
 //Is a function that runs when the instance of an object is created
         Gun.Fire();
+        Health.power = 100;
+        Health.message = "You are getting stronger.";
+        Ammo.message = "You have more ammo.";
+        Console.WriteLine(Health.power);
+        Console.WriteLine(Ammo.power);
     }
-
+    //Game PowerUps
+    public PowerUpBase Health = new PowerUpBase();
+    public PowerUpBase Ammo = new PowerUpBase();
     //Access Modifiers
-    private WeaponBase Gun = new WeaponBase(); //The object "Gun" is an object created, then telling WeaponBase file to run.
+    //Game weapons
+    private WeaponBase Gun = new WeaponBase(); //The var "Gun" is an object created, then telling WeaponBase file to run. -- The var retrieves its attributes from the WeaponBase class
+    private WeaponBase Rifle = new WeaponBase();
+    private WeaponBase Knife = new WeaponBase();
     public string name;
-    private int score;
+    private int score; // Without private, it would default to private.
     //-------------
 
 
